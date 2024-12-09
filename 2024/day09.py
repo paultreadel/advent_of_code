be64cf365bb6a2cfb32dd62d113f1d0402117247
Paul Readel <ReadelPaul@JohnDeere.com>
2024-12-09 11:18:53 -0600

    part 1 optimized

--- 2024/day09.py
+++ 2024/day09.py
@@ -1,93 +1,50 @@
-from collections import deque
-from copy import deepcopy
-
-from tqdm import tqdm
-
 if __name__ == "__main__":
     with open("data/day09.txt") as input_file:
         input_data = input_file.read()
 
-    # input_data = """2333133121414131402"""
+    input_data = """2333133121414131402"""
     # input_data = """12345"""
 
-    file_size_by_id = {}
-    free_spaces = []
-
+    disk = []
     file_id = 0
-    file_list = []
     for idx, char in enumerate(input_data):
-        if idx % 2 == 0:
-            file_size_by_id[file_id] = int(char)
-            file_list.extend([file_id] * int(char))
-            file_id += 1
+        if idx % 2:
+            disk.append((-1, int(char)))
         else:
-            free_spaces.append(int(char))
-            file_list.extend(["."] * int(char))
+            disk.append((file_id, int(char)))
+            file_id += 1
 
-    def sort_data(lst: deque):
-        sorted_files = []
-        while lst:
-            file_id = lst.popleft()
-            if isinstance(file_id, int):
-                sorted_files.append(file_id)
-            else:
-                while lst:
-                    next_file_id = lst.pop()
-                    if isinstance(next_file_id, int):
-                        sorted_files.append(next_file_id)
-                        break
-        return sorted_files
+    def print_disk(disk):
+        disk_as_chars = [str(file_id) if file_id >= 0 else "." for file_id in disk]
+        print("".join(disk_as_chars))
 
-    def next_contiguous_space_of_size(lst: deque, size: int):
-        data = deepcopy(lst)
-        idx_start = 0
-        while idx_start < len(data):
-            is_free = True
-            for idx in range(size):
-                if data[idx] != ".":
-                    is_free = False
-                    idx_start += 1
-                    data.rotate(-1)
-                    break
-            if is_free:
-                return idx_start
-        return -1
-
-    def sort_data_part_two(lst: deque, file_size_by_id: dict):
-        checked_sizes = set()
-        for _id, size in tqdm(
-            reversed(file_size_by_id.items()), total=len(file_size_by_id)
-        ):
-            if size in checked_sizes:
-                continue
-            idx_start = next_contiguous_space_of_size(lst, size)
-            if idx_start == 0:
-                idx_id_unsorted = lst.index(_id)
-                for idx in range(size):
-                    lst[idx_start + idx] = _id
-                    lst[idx_id_unsorted + idx] = "."
-            elif idx_start < 0:
-                checked_sizes.add(size)
-                continue
-            else:
-                idx_id_unsorted = lst.index(_id)
-                if idx_start > idx_id_unsorted:
+    def defrag(disk):
+        idx_head = 0
+        for idx_tail in reversed(range(len(disk))):
+            if idx_head >= idx_tail:
+                break
+            print_disk(disk)
+            for idx_head in range(idx_head, idx_tail):
+                id_tail = disk[idx_tail]
+                id_head = disk[idx_head]
+                if id_head >= 0:
+                    # space not free
                     continue
-                for idx in range(size):
-                    lst[idx_start + idx] = _id
-                    lst[idx_id_unsorted + idx] = "."
-        return lst
+                disk[idx_head], disk[idx_tail] = id_tail, id_head
+                break
 
-    sorted_files = sort_data(deque(file_list))
-    total_part_one = 0
-    for idx, file_id in enumerate(sorted_files):
-        total_part_one += file_id * idx
+    def disk_size(disk):
+        size = 0
+        for idx, file_id in enumerate(disk):
+            if file_id >= 0:
+                size += file_id * idx
+        return size
 
-    sorted_files = sort_data_part_two(deque(file_list), deepcopy(file_size_by_id))
-    total_part_two = 0
-    for idx, file_id in enumerate(sorted_files):
-        if isinstance(file_id, int):
-            total_part_two += file_id * idx
+    disk_unit_blocks = []
+    for file_id, size in disk:
+        disk_unit_blocks.extend([file_id] * size)
 
-    print(f"Part 1: {total_part_one}")
-    print(f"Part 2: {total_part_two}")
+    disk_defrag_part_one = defrag(disk_unit_blocks)
+    print(f"Part 1: {disk_size(disk_unit_blocks)}")
+
+    pass
