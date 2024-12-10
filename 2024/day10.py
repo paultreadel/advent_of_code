16b646743339a18f67bd3d508010e818981627c8
Paul Readel <ReadelPaul@JohnDeere.com>
2024-12-10 01:59:18 -0600

    simplify

--- 2024/day10.py
+++ 2024/day10.py
@@ -1,4 +1,6 @@
+from collections import deque
 from pprint import pprint  # noqa: F401
+from typing import List
 
 if __name__ == "__main__":
     with open("data/day10.txt") as input_file:
@@ -21,81 +23,34 @@ if __name__ == "__main__":
     ]
 
     grid = [list(row) for row in input_data.split("\n")]
-    pprint(grid)
-
-    # for all zeros
 
     def in_bounds(cell):
         return 0 <= cell[0] < len(grid) and 0 <= cell[1] < len(grid[0])
 
-    def print_scores(scores):
-        copy = scores.copy()
-        for i in range(len(scores)):
-            for j in range(len(scores[0])):
-                copy[i][j] = len(copy[i][j])
-        pprint(copy)
-
-    def cell_score(elevation, cell, scores, visited):
-        if cell in visited:
-            return scores[cell[0]][cell[1]]
+    def find_unique_trails(elevation, cell):
         if elevation == 9:
-            if cell in visited:
-                return 0
-            visited.add(cell)
-            scores[cell[0]][cell[1]] |= {cell}
-            return len(scores[cell[0]][cell[1]])
+            return [deque((cell,))]
+        new_trails: List[deque] = []
         for direction in DIRECTIONS:
             next_cell = (cell[0] + direction[0], cell[1] + direction[1])
             if not in_bounds(next_cell):
                 continue
-            try:
-                next_elevation = int(grid[next_cell[0]][next_cell[1]])
-            except ValueError:
-                next_elevation = -1
+            next_elevation = int(grid[next_cell[0]][next_cell[1]])
             if next_elevation == elevation + 1:
-                if cell_score(next_elevation, next_cell, scores, visited):
-                    scores[cell[0]][cell[1]] |= scores[next_cell[0]][next_cell[1]]
-        visited.add(cell)
-        return len(scores[cell[0]][cell[1]])
+                for trail in find_unique_trails(next_elevation, next_cell):
+                    trail.appendleft((cell,))
+                    new_trails.append(trail)
+        return new_trails
 
-    def unique_trails(elevation, cell, scores, visited):
-        if elevation == 9:
-            scores[cell[0]][cell[1]].add(tuple((cell,)))
-            return scores[cell[0]][cell[1]]
-        for direction in DIRECTIONS:
-            next_cell = (cell[0] + direction[0], cell[1] + direction[1])
-            if not in_bounds(next_cell):
-                continue
-            try:
-                next_elevation = int(grid[next_cell[0]][next_cell[1]])
-            except ValueError:
-                next_elevation = -1
-            if next_elevation == elevation + 1:
-                if unique_trails(next_elevation, next_cell, scores, visited):
-                    paths = []
-                    for path in scores[next_cell[0]][next_cell[1]]:
-                        paths.append((cell,) + path)
-                    scores[cell[0]][cell[1]].update(paths)
-        visited.add(cell)
-        return scores[cell[0]][cell[1]]
-
-    # array of all zeros same shape as grid
-    trailhead_count = 0
-    scores = [[set() for _ in range(len(grid[0]))] for _ in range(len(grid))]
-
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == "0":
-                trailhead_count += cell_score(0, (i, j), scores, set())
-    print(f"Part 1 {trailhead_count}")
-
-    # array of all zeros same shape as grid
-    trailhead_count = 0
+    total_trail_score = 0
+    total_trail_rating = 0
     scores = [[set() for _ in range(len(grid[0]))] for _ in range(len(grid))]
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == "0":
-                trails = unique_trails(0, (i, j), scores, set())
-                num_trails = len(set(tuple(l) for l in trails))
-                trailhead_count += num_trails
-    print(f"Part 2 {trailhead_count}")
+                unique_trails = find_unique_trails(0, (i, j))
+                trail_end_cells = set(trail[-1] for trail in unique_trails)
+                total_trail_score += len(trail_end_cells)
+                total_trail_rating += len(unique_trails)
+    print(f"Part 1: {total_trail_score}")
+    print(f"Part 2: {total_trail_rating}")
