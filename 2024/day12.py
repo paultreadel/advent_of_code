78538d4175b752cfebcc4352ddbfd675c168e032
Paul Readel <ReadelPaul@JohnDeere.com>
2024-12-12 00:51:28 -0600

    add part 2 solution

--- 2024/day12.py
+++ 2024/day12.py
@@ -5,10 +5,16 @@ if __name__ == "__main__":
     with open("data/day12.txt") as input_file:
         input_data = input_file.read()
 
-    input_data = """AAAA
-BBCD
-BBCC
-EEEC"""
+    #     input_data = """RRRRIICCFF
+    # RRRRIICCCF
+    # VVRRRCCFFF
+    # VVRCCCJFFF
+    # VVVVCJJCFE
+    # VVIVCCJJEE
+    # VVIIICJJEE
+    # MIIIIIJJEE
+    # MIIISIJEEE
+    # MMMISSJEEE"""
 
     grid = [list(row) for row in input_data.split("\n")]
 
@@ -57,9 +63,47 @@ EEEC"""
         perimeter = num_cells * 4 - num_edges
         return perimeter
 
-    # area == num_cells
+    def walk_grid_line(idx_walk, cells, step):
+        side_offset = step[::-1]
+        sides = 0
+        side_one_active, side_two_active = False, False
+        side_one_prev, side_two_prev = False, False
+        is_col = step == (1, 0)
+        size = len(grid) if is_col else len(grid[0])
+        for idx_step in range(size):
+            cell = (idx_step, idx_walk) if is_col else (idx_walk, idx_step)
+            if cell not in cells:
+                if not side_one_prev and side_one_active:
+                    sides += 1
+                if not side_two_prev and side_two_active:
+                    sides += 1
+                side_one_active, side_two_active = False, False
+                side_one_prev, side_two_prev = False, False
+                continue
+            cell_side_one = (cell[0] - side_offset[0], cell[1] - side_offset[1])
+            cell_side_two = (cell[0] + side_offset[0], cell[1] + side_offset[1])
+            side_one_active = cell_side_one not in cells
+            side_two_active = cell_side_two not in cells
+            if not side_one_prev and side_one_active:
+                sides += 1
+            if not side_two_prev and side_two_active:
+                sides += 1
+            side_one_prev = side_one_active
+            side_two_prev = side_two_active
+        return sides
+
+    def find_sides(cells):
+        sides = 0
+        for i in range(len(grid)):
+            num_sides = walk_grid_line(i, cells, (0, 1))
+            sides += num_sides
+        for j in range(len(grid[0])):
+            num_sides = walk_grid_line(j, cells, (1, 0))
+            sides += num_sides
+        return sides
 
     total_price_part_one = 0
+    total_price_part_two = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             cell = (i, j)
@@ -72,6 +116,8 @@ EEEC"""
             region_area = len(cells)
             region_perimeter = perimeter(region_area, region_edges)
             total_price_part_one += region_area * region_perimeter
+            num_sides = find_sides(cells)
+            total_price_part_two += region_area * num_sides
 
     print(f"Part 1: {total_price_part_one}")
-    # print(f"Part 2: {num_stones}")
+    print(f"Part 2: {total_price_part_two}")
