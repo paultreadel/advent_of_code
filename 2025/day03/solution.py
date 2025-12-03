f9401c2858a166708a450b1153c67a5b82227a52
Paul Readel <ReadelPaul@JohnDeere.com>
2025-12-03 01:30:15 -0600

    add day03, part2 success

--- 2025/day03/solution.py
+++ 2025/day03/solution.py
@@ -1,45 +1,48 @@
 import string
 
 filename = "sample.txt"
-# filename = "data.txt"
+filename = "data.txt"
 
 DEBUG = False
 
 
-def max_joltage_search(joltages, joltage_idx, total_joltage_value, joltage_cap="9"):
-    # exit if joltage index is at end of joltages
-    if joltage_idx >= len(joltages) - 1:
+def max_joltage_search(joltages, total_joltage_value, max_joltage_value=9):
+    # build max joltage from right to left, strategy is to find the largest number
+    # searching from right to left, repeating until a string of 12 characters is
+    # completed, if length of the remaining joltages ever drops below the number of
+    # characters required to reach 12 then backtracking is required by searching the
+    # next lowest digit
+    if not joltages:
         return False, total_joltage_value
-    original_joltage_idx = joltage_idx
-    for joltage in reversed(string.digits[1 : int(joltage_cap) + 1]):
-        if joltage not in joltages[joltage_idx:]:
+    if len(total_joltage_value) == 12:
+        return True, total_joltage_value
+    joltage_idx = None
+    for digit in string.digits[max_joltage_value:0:-1]:
+        joltage = str(digit)
+        try:
+            joltage_idx = joltages.index(joltage)
+        except ValueError:
             continue
-        joltage_idx = original_joltage_idx
-        joltage_idx = joltages[joltage_idx:].index(joltage) + joltage_idx
-        total_joltage_value += joltages[joltage_idx]
-        joltage_idx += 1
-        if len(total_joltage_value) == 12:
-            return True, total_joltage_value
-        search_status, total_joltage_value = max_joltage_search(
-            joltages, joltage_idx, total_joltage_value
-        )
-        if search_status:
-            break
-        # search failed, reset back to original index, pop failed digit off list and
-        # continue to next lowest number
-        joltage_idx = original_joltage_idx
-        total_joltage_value = total_joltage_value[:-1]
-    while len(total_joltage_value) != 12:
-        joltage_cap = str(int(total_joltage_value[-1]) - 1)
-        original_joltage_idx -= 1
-        joltage_idx = original_joltage_idx
-        total_joltage_value = total_joltage_value[:-1]
-        search_status, total_joltage_value = max_joltage_search(
-            joltages, joltage_idx, total_joltage_value, joltage_cap
-        )
-        if search_status:
-            break
-    return True, total_joltage_value
+        total_joltage_value += joltage
+        break
+
+    if joltage_idx is None:
+        return False, total_joltage_value
+    search_status, total_joltage_value = max_joltage_search(
+        joltages[joltage_idx + 1 :], total_joltage_value
+    )
+    if search_status:
+        return True, total_joltage_value
+    # search failed backtrack, but cap search for joltage one less than the value
+    # that failed
+    if len(total_joltage_value) == 12:
+        return True, total_joltage_value
+
+    total_joltage_value = total_joltage_value[:-1]
+    search_status, total_joltage_value = max_joltage_search(
+        joltages, total_joltage_value, int(joltage) - 1
+    )
+    return search_status, total_joltage_value
 
 
 if __name__ == "__main__":
@@ -59,11 +62,13 @@ if __name__ == "__main__":
                 continue
             max_val = max(joltages[joltage_idx + 1 :])
             bank_max_joltage_candidates.append(int(str(joltage) + str(max_val)))
-            print(joltages, bank_max_joltage_candidates)
+            if DEBUG:
+                print(joltages, bank_max_joltage_candidates)
         total_part1 += max(bank_max_joltage_candidates)
 
-        _, total_joltage_value = max_joltage_search(joltage_bank, 0, "")
-        print(total_joltage_value)
+        _, total_joltage_value = max_joltage_search(joltage_bank, "")
+        if DEBUG:
+            print(total_joltage_value)
         total_part2 += int(total_joltage_value)
     print(f"Part 1: {total_part1}")
     print(f"Part 1: {total_part2}")
